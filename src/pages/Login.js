e09a657682ae221daf1b184f2ca451b683d831a4
Tate Keller <tatek1993@gmail.com>
2020-02-04 15:29:30 -0600

    tried adding ternary operator to direct the login link

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,8 @@ import * as Yup from 'yup';
 import axios from 'axios';
 import shortid from 'shortid';
 
+// let database = ""
+
 const Login =({values, errors, touched, status}) =>{
 
     const [usersLogin, setUsersLogin] = useState([]);
@@ -45,10 +47,10 @@ const Login =({values, errors, touched, status}) =>{
                     Role:
                 </label>
                 <br/>
-                <Field  as="select" id="role" type="text" name="role" >
+                <Field  as="select" id="role" name="role" >
                     <option disabled>Choose an Option</option>
-                    <option value="Looking to Post Jobs">Looking to Post Jobs</option>
-                    <option value="Looking For Jobs">Looking For Jobs</option>
+                    <option value="Looking to Post Jobs" >Looking to Post Jobs</option>
+                    <option value="Looking For Jobs" >Looking For Jobs</option>
                 </Field>
                 {touched.role && errors.role && (
                 <p className="errors" style={{color: "red", fontSize:"10px"}}>{errors.role}</p>
@@ -56,7 +58,7 @@ const Login =({values, errors, touched, status}) =>{
                  <br/>
                  <br/>
 
-                <button type="submit" onClick={event =>  window.location.href="/"}>
+                <button type="submit">
                     Submit
                 </button>
             </Form>
@@ -64,7 +66,7 @@ const Login =({values, errors, touched, status}) =>{
     );
 };
 
-const MakeFormikFormOutOfComponent = withFormik({
+const FormikLoginForm = withFormik({
     mapPropsToValues({
         email,
         password,
@@ -72,10 +74,10 @@ const MakeFormikFormOutOfComponent = withFormik({
     }) {
         console.log('asd');
         return {
-            id: shortid.generate(),
-            email: "",
-            password: "",
-            role: ""
+            // id: shortid.generate(),
+            email: email || "",
+            password: password || "",
+            role: role || ""
         };
     },
     validationSchema: Yup.object().shape({
@@ -91,22 +93,24 @@ const MakeFormikFormOutOfComponent = withFormik({
             .required("Password is Required.")    
 
     }),
-    handleSubmit(values, {resetForm, setValues}) {
+    handleSubmit(values, {props, resetForm, setValues}) {
         console.log("submitting", values);
         axios
         .post(
-            "",
+            (values.role === "Looking to Post Jobs") ? "https://droom-4.herokuapp.com/api/auth/companies/login" : "https://droom-4.herokuapp.com/api/auth/users/login"
+            ,
             values
         )
         .then(res => {
             console.log('success', res)
+            props.history.push("/Dashboard")
             resetForm();
         })
-        .catch(err => console.log(err)
+        .catch(err => alert("Login failed", err)
         );
     }
 });
 
-const FormikLogin = MakeFormikFormOutOfComponent(Login);
+const FormikLogin = FormikLoginForm(Login);
 
 export default FormikLogin
