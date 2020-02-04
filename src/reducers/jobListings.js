4b2dea4e59cfe5b49de366719a070dd9b39d34c2
Nolan Picini <nolanpic@gmail.com>
2020-02-03 18:33:05 -0800

    git status

---
+++ src/reducers/jobListings.js
@@ -0,0 +1,22 @@
+const initialState = [];
+
+/*
+    Each array item will look like:
+    {
+        id: 0,
+        company_id: 0,
+        job_title: '',
+        expiry_date: '',
+        job_detail: '',
+        matching_skill: '',
+        created_at: '',
+        updated_at: ''
+    }
+*/
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
