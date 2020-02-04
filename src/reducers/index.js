edd29474eae381631958821208f6b344f2140790
Nolan Picini <nolanpic@gmail.com>
2020-02-03 19:51:47 -0800

    Add all reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from 'redux';
 import loggedInUser from './loggedInUser';
 import loggedInCompany from './loggedInCompany';
+import userListings from './userListings';
+import jobListings from './jobListings';
+import loading from './loading';
+import error from './error';
 
 export default combineReducers({
     loggedInUser,
-    loggedInCompany
+    loggedInCompany,
+    userListings,
+    jobListings,
+    loading,
+    error
 });
 
