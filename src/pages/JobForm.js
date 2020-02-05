a2bdd6714b3f2b3850811d9da06a102dac521166
Tate Keller <tatek1993@gmail.com>
2020-02-05 15:01:19 -0600

    changed error styling

--- src/pages/JobForm.js
+++ src/pages/JobForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const JobContainer = styled.div `
     width: 30vw;
@@ -23,6 +23,7 @@ export const Error = styled.p `
     font-weight: normal;
     line-height: 0.5;
 `
+
 
 const JobForm = ({ values, errors, touched, status }) => {
 
