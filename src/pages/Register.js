9bbc288826bd55bf1909c223ff42c4d50a7000f9
Nolan Picini <nolanpic@gmail.com>
2020-02-05 16:55:30 -0800

    Comment out conflicting styled components

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -66,37 +66,37 @@ let requiredToggledFields = null;
 //     font-size: 10px;
 //     line-height: 0.5;
 // `
-export const RegisterHeader = styled.h1 `
-    font-weight: bolder;
-    color: #263D42;
-    text-shadow: 1px 1px #63C7B2;
-    font-family: 'Alatsi', sans-serif;
-`
-export const LoginLink = styled.a `
-    font-size: 1rem;
-    letter-spacing: 1px;
-    color: #263D42;
-    &:hover {
-        color: #63C7B2;
-      }
+// export const RegisterHeader = styled.h1 `
+//     font-weight: bolder;
+//     color: #263D42;
+//     text-shadow: 1px 1px #63C7B2;
+//     font-family: 'Alatsi', sans-serif;
+// `
+// export const LoginLink = styled.a `
+//     font-size: 1rem;
+//     letter-spacing: 1px;
+//     color: #263D42;
+//     &:hover {
+//         color: #63C7B2;
+//       }
     
-`
-export const RegisterButton = styled.button `
-    width: 100px;
-    height: 35px;
-    border-radius: 5px;
-    background-color: #63C7B2;
-    border: 5px outset #2A9D8F;
-    font-size: 1rem;
-    font-weight: bold;
-    color: #263D42;
-    &:hover {
-        background: #2A9D8F;
-        border: 5px inset #63C7B2;
-      }
-    margin-bottom: 10px;  
-`
-;
+// `
+// export const RegisterButton = styled.button `
+//     width: 100px;
+//     height: 35px;
+//     border-radius: 5px;
+//     background-color: #63C7B2;
+//     border: 5px outset #2A9D8F;
+//     font-size: 1rem;
+//     font-weight: bold;
+//     color: #263D42;
+//     &:hover {
+//         background: #2A9D8F;
+//         border: 5px inset #63C7B2;
+//       }
+//     margin-bottom: 10px;  
+// `
+// ;
 
 const UserForm = ({ values, errors, touched, status }) => {
 
